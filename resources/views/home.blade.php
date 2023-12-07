7dcddef855c7c88da4d2a253ad16d340775cdba3
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-07 16:45:46 +0100

    ADD menu header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <menu>
+            <ul>
+                <li>
+                    <a href="#">home</a>
+                </li>
+                <li>
+                    <a href="/about">about</a>
+                </li>
+                <li>
+                    <a href="/product">product</a>
+                </li>
+            </ul>
+        </menu>
+    </header>
     <h1>{{ $title }}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,17 @@ Route::get('/', function () {
     
     return view('home', compact('title'));
 });
+
+Route::get('/about', function () {
+    
+    $about = 'Questo pagina - About';
+    $go_home = 'Go at Home';
+    return view('about', compact('about','go_home'));
+});
+
+Route::get('/product', function () {
+    
+    $product = 'Questo pagina - product';
+    $go_home = 'Go at Home';
+    return view('product', compact('product','go_home'));
+});
