ae01a86666ea540873788fe99bf92135a7ba420a
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-07 17:24:42 +0100

    delete variable go_home

--- routes/web.php
+++ routes/web.php
@@ -27,13 +27,13 @@ Route::get('/', function () {
 Route::get('/about', function () {
     
     $about = 'Questo pagina - About';
-    $go_home = 'Go at Home';
+   
     return view('about', compact('about','go_home'));
 })->name('about');
 
 Route::get('/product', function () {
     
     $product = 'Questo pagina - product';
-    $go_home = 'Go at Home';
+ 
     return view('product', compact('product','go_home'));
 })->name('product');;
